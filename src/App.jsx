f0d5c7f858ca49e15862f9ea94a91aad5506aa46
Vishal Saini <Vishalsaini9675@gmail.com>
2025-03-06 10:31:50 +0530

    remove comment

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   useEffect(() => {
     document.querySelector("html").classList.remove("light", "dark");
-    document.querySelector("html").classList.add(themeMode)
+    document.querySelector("html").classList.add(themeMode);
   }, [themeMode]);
 
   return (
@@ -26,12 +26,10 @@ function App() {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* theme button  */}
             <ThemeBtn />
           </div>
 
           <div className="w-full max-w-sm mx-auto">
-            {/* card */}
             <Card />
           </div>
         </div>
