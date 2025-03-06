0df170d0e938b9e7fe37936d52b2cbaef4dc85cc
Vishal Saini <Vishalsaini9675@gmail.com>
2025-03-06 10:29:05 +0530

    Frist commit of themeswitcher

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./context/theme";
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // actual chnage in theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode)
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* theme button  */}
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            {/* card */}
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
